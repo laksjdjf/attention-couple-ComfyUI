fdd09235ee998f2d486bab90ed06a4f67ebd4230
laksjdjf <aikasama@hotmail.co.jp>
2023-07-21 16:54:53 +0900

    色々修正

--- attention_couple.py
+++ attention_couple.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import copy
 
 def get_masks_from_q(masks, q):
     ret_masks = []
@@ -9,7 +10,6 @@ def get_masks_from_q(masks, q):
             down_sample_rate = int((mask_size // 64 // q.shape[1]) ** (1/2))
             mask_downsample = F.interpolate(mask.unsqueeze(0), scale_factor= 1/8/down_sample_rate, mode="nearest").squeeze(0)
             mask_downsample = mask_downsample.view(1,-1, 1).repeat(q.shape[0], 1, q.shape[2])
-            mask_downsample = mask_downsample.to(q.device, dtype=q.dtype)
             ret_masks.append(mask_downsample)
         else: # coupling処理なしの場合
             ret_masks.append(torch.ones_like(q))
@@ -24,36 +24,47 @@ class AttentionCouple:
                 "model": ("MODEL", ),
                 "positive": ("CONDITIONING",),
                 "negative": ("CONDITIONING",),
+                "mode": (["Attention", "Latent"], ),
             }
         }
     RETURN_TYPES = ("MODEL", "CONDITIONING", "CONDITIONING")
     FUNCTION = "attention_couple"
     CATEGORY = "loaders"
 
-    def attention_couple(self, model, positive, negative):
+    def attention_couple(self, model, positive, negative, mode):
+        if mode == "Latent":
+            return (model, positive, negative)
         self.negative_positive_masks = []
         self.negative_positive_conds = []
-
+        
+        new_positive = copy.deepcopy(positive)
+        new_negative = copy.deepcopy(negative)
+        
+        dtype = model.model.diffusion_model.dtype
+        device = "cuda"
+        
         # maskとcondをリストに格納する
-        for conditions in [negative, positive]:
+        for conditions in [new_negative, new_positive]:
             conditions_masks = []
             conditions_conds = []
             if len(conditions) != 1:
-                mask_norm = torch.stack([cond[1]["mask"] * cond[1]["mask_strength"] for cond in conditions])
+                mask_norm = torch.stack([cond[1]["mask"].to(device, dtype=dtype) * cond[1]["mask_strength"] for cond in conditions])
                 mask_norm = mask_norm / mask_norm.sum(dim=0) # 合計が1になるように正規化
                 conditions_masks.extend([mask_norm[i] for i in range(mask_norm.shape[0])])
-                conditions_conds.extend([cond[0] for cond in conditions])
+                conditions_conds.extend([cond[0].to(device, dtype=dtype) for cond in conditions])
                 del conditions[0][1]["mask"] # latent coupleの無効化
                 del conditions[0][1]["mask_strength"]
             else:
                 conditions_masks = [False]
-                conditions_conds = [conditions[0][0]]
+                conditions_conds = [conditions[0][0].to(device, dtype=dtype)]
             self.negative_positive_masks.append(conditions_masks)
             self.negative_positive_conds.append(conditions_conds)
-
-        self.hook_forwards(model.model.diffusion_model)
+        self.conditioning_length = (len(new_negative), len(new_positive))
+            
+        new_model = copy.deepcopy(model)
+        self.hook_forwards(new_model.model.diffusion_model)
         
-        return (model, [positive[0]], [negative[0]]) # pool outputは・・・後回し
+        return (new_model, [new_positive[0]], [new_negative[0]]) # pool outputは・・・後回し
     
     def hook_forwards(self, root_module: torch.nn.Module):
         for name, module in root_module.named_modules():
@@ -63,27 +74,36 @@ class AttentionCouple:
     def hook_forward(self, module):
         def forward(x, context=None, value=None, mask=None):
             q = module.to_q(x)
-            outs = []
-            # negative, positiveの順に処理。
-            for i in range(2):
-                q_cond = q.chunk(2)[i]
-                b = q_cond.shape[0]
-                out = torch.zeros_like(q_cond)
-                masks = get_masks_from_q(self.negative_positive_masks[i], q_cond)
-                q_cond = q_cond.view(b, -1, module.heads, module.dim_head).transpose(1, 2)
-                for mask, context in zip(masks, self.negative_positive_conds[i]):
-                    k = module.to_k(context.to(q_cond.device, dtype=q_cond.dtype)).repeat(b, 1, 1)
-                    v = module.to_v(context.to(q_cond.device, dtype=q_cond.dtype)).repeat(b, 1, 1)
-                    
-                    k = k.view(b, -1, module.heads, module.dim_head).transpose(1, 2)
-                    v = v.view(b, -1, module.heads, module.dim_head).transpose(1, 2)
+            
+            len_neg, len_pos = self.conditioning_length
+            q_uncond, q_cond = q.chunk(2)
+            b = q_cond.shape[0]
+            
+            masks_uncond = get_masks_from_q(self.negative_positive_masks[0], q_uncond)
+            masks_cond = get_masks_from_q(self.negative_positive_masks[1], q_cond)
+            masks = torch.cat(masks_uncond + masks_cond)
 
-                    qkv = torch.nn.functional.scaled_dot_product_attention(q_cond, k, v, attn_mask=None, dropout_p=0.0, is_causal=False)
-                    qkv = qkv.transpose(1, 2).reshape(b, -1, module.heads * module.dim_head)
-                    out += qkv * mask
-                outs.append(out)
+            q_target= torch.cat([q_uncond]*len_neg + [q_cond]*len_pos, dim=0)
+            q_target = q_target.view(b*(len_neg+len_pos), -1, module.heads, module.dim_head).transpose(1, 2)
+            context_uncond = torch.cat([cond.repeat(b,1,1) for cond in self.negative_positive_conds[0]], dim=0)
+            context_cond = torch.cat([cond.repeat(b,1,1) for cond in self.negative_positive_conds[1]], dim=0)
 
-            return module.to_out(torch.cat(outs)) # uncond, cond 全てのサンプラーがこの順番なことを祈る
+            context = torch.cat([context_uncond, context_cond], dim=0)
+
+            k = module.to_k(context)
+            v = module.to_v(context)
+            
+            k = k.view(b * (len_neg + len_pos), -1, module.heads, module.dim_head).transpose(1, 2)
+            v = v.view(b * (len_neg + len_pos), -1, module.heads, module.dim_head).transpose(1, 2)
+
+            qkv = torch.nn.functional.scaled_dot_product_attention(q_target, k, v, attn_mask=None, dropout_p=0.0, is_causal=False)
+            qkv = qkv.transpose(1, 2).reshape(b * (len_neg + len_pos), -1, module.heads * module.dim_head)
+            qkv = qkv * masks
+            
+            out_uncond = qkv[:len_neg*b].view(len_neg, b, -1, module.heads * module.dim_head).sum(dim=0)
+            out_cond = qkv[len_neg*b:].view(len_pos, b, -1, module.heads * module.dim_head).sum(dim=0)
+            out = torch.cat([out_uncond, out_cond], dim=0) # uncond, cond 全てのサンプラーがこの順番なことを祈る
+            return module.to_out(out)
         return forward
         
 NODE_CLASS_MAPPINGS = {
